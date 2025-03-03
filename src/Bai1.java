812304645af3c08470914860ccd69232ad140516
thecuongk5hp <theccuong2k5hp@gmail.com>
2025-03-04 05:43:02 +0700

    session03

---
+++ src/Bai1.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap mot so nguyen: ");
+        int number = scanner.nextInt();
+        if (number == 0) {
+            System.out.println(number + "la So khong phai chan cung khong phai le");
+        }  else if (number % 2 == 0) {
+            System.out.println(number + "la so le");
+        } else {
+            System.out.println(number + "la so le");
+        }
+        scanner.close();
+    }
+}
